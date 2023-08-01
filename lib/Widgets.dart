139852da1929e2c7c251339371ce940ffce773cd
kashaf172725 <134833348+kashaf172725@users.noreply.github.com>
2023-07-31 22:12:24 -0700

    ...............

--- lib/Widgets.dart
+++ lib/Widgets.dart
@@ -1,5 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_application_1/utils/constants.dart';
+
+class MyCircle extends StatefulWidget {
+  const MyCircle({super.key});
+
+  @override
+  State<MyCircle> createState() => _MyCircleState();
+}
+
+class _MyCircleState extends State<MyCircle> {
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: CircleAvatar(
+                        radius: 45,
+                      ),
+    );
+  }
+}
+
+
+
+
 
 class MySqure extends StatefulWidget {
   const MySqure({super.key});
