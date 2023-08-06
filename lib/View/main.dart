471db96177612993d8b8458e40e6015a9d1f425b
kashaf172725 <134833348+kashaf172725@users.noreply.github.com>
2023-08-05 23:27:46 -0700

    ........

--- lib/View/main.dart
+++ lib/View/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
     theme: ThemeData(primarySwatch: Colors.purple),
-    home :TextFeild(),
+    home :TextF eild(),
 
 
 
